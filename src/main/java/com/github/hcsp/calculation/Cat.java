fcdb3cfee9a3da31e401b81080a7053bdfa39f6c
sunkuangdong <769296817@qq.com>
2019-11-30 22:37:59 +0800

    fix: 处理属性为false的情况

--- src/main/java/com/github/hcsp/calculation/Cat.java
+++ src/main/java/com/github/hcsp/calculation/Cat.java
@@ -15,7 +15,7 @@ public class Cat {
      * @param cute true为萌，false为不萌
      */
     public void setCute(boolean cute) {
-        if (cute) properties = properties | CUTE;
+        properties = cute ? (CUTE | properties) : (~CUTE & properties);
     }
 
     /**
@@ -33,7 +33,7 @@ public class Cat {
      * @param fat true为胖，false为不胖
      */
     public void setFat(boolean fat) {
-        if (fat) properties = properties | FAT;
+        properties = fat ? (FAT | properties) : (~FAT & properties);
     }
 
     /**
@@ -51,7 +51,7 @@ public class Cat {
      * @param white true为白，false为不白
      */
     public void setWhite(boolean white) {
-        if (white) properties = properties | WHITE;
+        properties = white ? (WHITE | properties) : (~WHITE & properties);
     }
 
     /**
